13b2554aec4a56a0a8564430613087ef54fc4b12
Alinur <anarasikk.gmail.com>
2022-06-09 16:41:56 +0600

    13 lesson

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+    <header className="header">
+    <img src="https://logoeps.com/wp-content/uploads/2014/09/37150-apple-logo-icon-vector-icon-vector-eps.png"/>
+    </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+
+
+const Profile = () => {
+    return (
+      <div className="content">
+      <img src="https://html5css.ru/css/img_lights.jpg" /> 
+      <div>
+      ava + description 
+      </div>
+      <div>
+      my posts
+      <div> new posts </div>
+      <div>
+      <div>post 1</div>
+      <div>post 2</div>
+      </div>
+      </div>
+      </div>
+     
+    )
+}
+
+export default Profile;
